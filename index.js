fe62a851ea4ee1322b1daf53e361716e33ffc6f2
Igor <igorsalomao13@gmail.com>
2023-10-27 19:19:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,15 +34,15 @@ const handleGameOver = () => {
 const changeDirection = (e) => {
     if (e.key === "ArrowUp" && velocityY !== 1) {
         velocityX = 0;
-        velocityY = -0.5;
+        velocityY = -05;
     } else if (e.key === "ArrowDown" && velocityY !== -1) {
         velocityX = 0;
-        velocityY = 0.5;
+        velocityY = 05;
     } else if (e.key === "ArrowLeft" && velocityX !== 1) {
-        velocityX = -0.5;
+        velocityX = -05;
         velocityY = 0;
     } else if (e.key === "ArrowRight" && velocityX !== -1) {
-        velocityX = 0.5;
+        velocityX = 05;
         velocityY = 0;
     }
 };
